4d8ec4b5d6dde23c9745c5ee42dbbf8dd152aa2f
chris <christianwilliamsjire@gmail.com>
2019-09-07 16:22:43 +0300

    install bootstap,newsapi and add data on __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_bootstap import Bootsrap
+from config import config_options
+
+bootstrap = Bootsrap()
+
+def create_app(config_name):
+    
+    app = Flask(__name__)
+    
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    
+    # Initializing Flask Extensions
+    bootstrap.init_app(app)
+    
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
