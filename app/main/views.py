2d4b34648475e83171cea37115e2c12b681451bf
chris <christianwilliamsjire@gmail.com>
2019-09-07 20:15:47 +0300

    this commit will import sources and articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..request import get_news
+from ..requests import get_sources,get_articles
+from ..models import Sources
 
 # Views
 @main.route('/')
