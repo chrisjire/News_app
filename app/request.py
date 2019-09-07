0778da3d8c942715931f07ceb6616ef6b547ef74
chris <christianwilliamsjire@gmail.com>
2019-09-07 21:00:36 +0300

    this commit will create get_articles function

--- app/request.py
+++ app/request.py
@@ -36,7 +36,7 @@ def get_sources(category):
 	return sources_results
 
 def process_sources(sources_list):
-    	'''
+    '''
 	Function that processes the news sources results and turns them into a list of objects
 	Args:
 		sources_list: A list of dictionaries that contain sources details
@@ -60,3 +60,19 @@ def process_sources(sources_list):
 
 
 	return sources_results
+
+def get_articles(id):
+    '''
+	Function that processes the articles and returns a list of articles objects
+	'''
+	get_articles_url = articles_url.format(id,api_key)
+
+	with urllib.request.urlopen(get_articles_url) as url:
+		articles_results = json.loads(url.read())
+
+
+		articles_object = None
+		if articles_results['articles']:
+			articles_object = process_articles(articles_results['articles'])
+
+	return articles_object
